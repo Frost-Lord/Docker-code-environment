69e5af3c92fd358aa80b18b0edaec1c2a4c32b5c
Frost Lord <51778028+DaKing01@users.noreply.github.com>
2022-10-03 15:23:22 +1100

    .

--- testCode/index.js
+++ testCode/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const port = process.env.port;
+const port = process.env.PORT;
+console.log(process.env.PORT)
+console.log(process.env.port)
+console.log(process.env)
 
 app.get('/', (req, res) => {
   res.send('Hello World! lol this is dockerised')
